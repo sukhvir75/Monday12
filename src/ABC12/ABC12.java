4ead1e54a8857d1052fcfc4b37964eab63441355
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-03 00:50:29 -0400

    adcvfg

--- src/ABC12/ABC12.java
+++ src/ABC12/ABC12.java
@@ -4,6 +4,9 @@ public class ABC12 {
 	public static void main(String args []) {
 		System.out.println("Today is monday");
 	}
+	public static void add(int a,int b) {
+		System.out.println(a+b);
+	}
 	public static void subtract(int a,int b) {
 		System.out.println(a-b);
 	}
