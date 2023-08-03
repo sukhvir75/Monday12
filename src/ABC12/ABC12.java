1e3a26e0f504664e9529650a06c5b2c1af54e322
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-03 00:45:14 -0400

    Update ABC12.java

--- src/ABC12/ABC12.java
+++ src/ABC12/ABC12.java
@@ -4,5 +4,7 @@ public class ABC12 {
 	public static void main(String args []) {
 		System.out.println("Today is monday");
 	}
-
+	public static void subtract(int a,int b) {
+		System.out.println(a-b);
+	}
 }
